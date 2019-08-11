a7700ee37c6ace0b6d9e85392b8d45e4ccc1ebe4
Rafael Urbina <rafaelurbinan@hotmail.com>
2019-08-11 16:03:16 -0430

    Server Initial Config

---
+++ server/.eslintrc.js
@@ -0,0 +1,49 @@
+module.exports = {
+    "env": {
+        "commonjs": true,
+        "es6": true,
+        "node": true,
+		"mongo": true,
+		"jest": true 
+    },
+    "extends": "eslint:recommended",
+    "globals": {
+        "Atomics": "readonly",
+        "SharedArrayBuffer": "readonly"
+    },
+    "parserOptions": {
+        "ecmaVersion": 2018
+    },
+    "plugins": ["babel"],
+    "rules": {
+        "arrow-parens":"off",
+		"comma-dangle":"off",
+		"function-paren-newline":"off",
+		"max-len":[1,120],
+		"no-param-reassign":"off",
+		"reaact/require-default-props":"off",
+		"linebreak-style":["off","windows"],
+		"react/prefer-stateless-function":["off"],
+		"padded-blocks":["off"],
+		"no-tabs":["off"],
+		"indent":["warn","tab"],
+		"react/jsx-indent":["warn",'tab'],
+		"no-trailing-spaces":["off"],
+		"react/jsx-one-expression-per-line":["off"],
+		"no-unused-vars":["warn"],
+		"react/destructuring-assignment":["off"],
+		"react/no-access-state-in-setstate":["warn"],
+		"arrow-body-style":["warn","always"],
+		"no-multiple-empty-lines":["warn"],
+		"func-names":["off"],
+		"prefer-arrow-callback":["warn"],
+		"no-underscore-dangle":["off"],
+		"prefer-template": ["off"],
+		"no-plusplus": ["off"],
+		"max-len": ["off"],
+		"react/prop-types": ["off"],
+		"arrow-body-style": ["off"],
+		"jsx-a11y/label-has-associated-control": ["off"],
+		"jsx-a11y/label-has-for": ["off"]	
+    }
+};
---
+++ server/src/config/config.js
@@ -0,0 +1,61 @@
+require('custom-env').env(true, './../../');
+const randomstring = require('randomstring');
+const path = require('path');
+
+const nconf = require('nconf');
+
+
+nconf.argv()
+    .env()
+    .file( { file: path.join(__dirname + './../../' + 'config.json')});
+
+nconf.set('RANDOM_STRING10', randomstring.generate(10));
+
+nconf.defaults({
+
+        DB_USER: "root",
+        DB_PASS: null,
+        User_Block_Duration_Minutes: 10,
+        User_Wrong_Passwords_Attemps: 5
+
+});
+
+const env = nconf.get('NODE_ENV') || 'development';
+
+let mongoUri = nconf.get('MONGODB_URI');
+
+if (env === 'development') {
+    mongoUri = nconf.get('MONGODB_URI') + '/' + nconf.get('DB_NAME');
+} else if (env === 'test') {
+    mongoUri = nconf.get('MONGODB_URI') + '/' + nconf.get('DB_NAME') + '-Test';
+}
+
+console.log('env *****', env);
+console.log('PORT = ', nconf.get('PORT'));
+console.log('MONGODB_URI = ', mongoUri);
+console.log('RANDOM_STRING = ', nconf.get('RANDOM_STRING10'));
+console.log('USER_BLOCK_DURATION_MINUTES = ', nconf.get('User_Block_Duration_Minutes'));
+console.log('USER_WRONG_PASSWORDS_ATTEMPS = ', nconf.get('User_Wrong_Passwords_Attemps'));
+
+module.exports = {
+
+    env: {
+        NODE_ENV: env,
+        MONGODB_URI: mongoUri,
+        PORT: nconf.get('PORT'),
+    },
+    general: {
+
+    },
+    db: {
+        DB_USER: nconf.get('DB_USER'),
+        DB_PASS: nconf.get('DB_PASS')        
+    },
+    user: {
+        USER_BLOCK_DURATION_MINUTES: nconf.get('User_Block_Duration_Minutes'),
+        USER_WRONG_PASSWORDS_ATTEMPS: nconf.get('User_Wrong_Passwords_Attemps')
+    }
+};
+
+
+
